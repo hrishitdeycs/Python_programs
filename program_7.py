b315b90aa2069ec0fa29f8f38b2fdba4c81a2232
Hrishit Dey <hrishitdey@gmail.com>
2025-06-26 20:11:57 +0530

    Create program_7.py

---
+++ program_7.py
@@ -0,0 +1,16 @@
+def occurances(a,b):
+    newlist =[]
+    if b not in a:
+       print(-1)
+    else:
+        i= 0
+        while i< len(a):
+            c=a.find(b,i)
+            if c== -1:
+              break
+            i= c+ len(b)
+            newlist.append(c)
+        print(newlist)
+a= input("enter first string :")
+b= input("enter second string :")
+occurances(a,b)
