0796f115b156cc4bf71448f0aed866c178efa4ed
Hrishit Dey <hrishitdey@gmail.com>
2025-06-26 20:05:41 +0530

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -8,3 +8,29 @@ if n>1:
       print(n,"prime number")
 else:
     print(n,"not a prime number ")
+n = eval(input("enter value "))
+for num in range(1,n):
+    if num > 1:
+        for i in range (2,num):
+           if num % i ==  0:
+            break
+        else:
+            print(num,end =',')
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+def generate_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+n=int(input("Enter number of prime numbers :"))
+print(generate_primes(n))
+
